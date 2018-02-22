b5e9d7dbfcf08df9627930d67cef0367bcb6b817
JustChief <cdp4120tt@gmail.com>
2018-02-22 18:21:29 -0500

    Hot Towel Angular By John Papa
    School App

---
+++ SchoolMeApp/HotTowelAngular/app/config.route.js
@@ -0,0 +1,66 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app');
+    var appRoot = ""; //http://awsschoolmeappv2.us-east-1.elasticbeanstalk.com/
+
+    // Collect the routes
+    app.constant('routes', getRoutes());
+    
+    // Configure the routes and route resolvers
+    app.config(['$routeProvider', 'routes', routeConfigurator]);
+    function routeConfigurator($routeProvider, routes) {
+
+        routes.forEach(function (r) {
+            $routeProvider.when(r.url, r.config);
+        });
+        $routeProvider.otherwise({ redirectTo: '/' });
+    }
+
+    // Define the routes 
+    function getRoutes() {
+        return [
+            {
+                url: '/',
+                config: {
+                    templateUrl: appRoot + 'app/home/home.html',
+                    title: 'home',
+                    settings: {
+                        nav: 1,
+                        content: '<i class="fa fa-home"></i> Home'
+                    }
+                }
+            }, {
+                url: '/dashboard',
+                config: {
+                    templateUrl: appRoot + 'app/dashboard/dashboard.html',
+                    title: 'dashboard',
+                    settings: {
+                        nav: 2,
+                        content: '<i class="fa fa-dashboard"></i> Dashboard'
+                    }
+                }
+            }, {
+                url: '/admin',
+                config: {
+                    title: 'admin',
+                    templateUrl: appRoot + 'app/admin/admin.html',
+                    settings: {
+                        nav: 3,
+                        content: '<i class="fa fa-lock"></i> Admin'
+                    }
+                }
+            }, {
+                url: '/teachers',
+                config: {
+                    title: 'teachers',
+                    templateUrl: appRoot + 'app/Teachers/teachers.html',
+                    settings: {
+                        nav: 4,
+                        content: '<i class="fa fa-user"></i> Teachers'
+                    }
+                }
+            }
+        ];
+    }
+})();
---
+++ SchoolMeApp/HotTowelAngular/app/home/home.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'home';
+    angular.module('app').controller(controllerId, ['common', '$uibModal', '$uibModalInstance', home]);
+
+    function home(common, $uibModal, $uibModalInstance) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'My Home, This means it worked, For real this time';
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function () { log('Activated New View'); });
+        }
+
+        $("#register").click(function () {
+            var modalInstance = $uibModal.open({
+                templateUrl: 'http://awsschoolmeappv2.us-east-1.elasticbeanstalk.com/app/home/modals/checkUser.html',
+                controller: 'checkUser as vm',
+                size: 'lg',
+                backdrop: 'static'
+            });
+
+            modalInstance.result.then(function (data) {
+
+            }, function () {
+                log("Modal Dismissed");
+            });
+
+        });
+
+    }
+})();
